71de08bd87ff4a170231aef57b806b460140d90e
mjones <matthew7j@aol.com>
2015-04-17 16:06:27 -0400

    Fixing Year Adding Only

--- src/DatabaseEngine.java
+++ src/DatabaseEngine.java
@@ -43,7 +43,7 @@ public class DatabaseEngine
             for (int i = 0; i < e.years.size(); i++)
             {
                 if (!years.contains(e.years.get(i))) {
-                    addYearData(e.years.get(i), e);
+                    addOldYearData(e.years.get(i), e);
                 }
             }
         }
@@ -55,7 +55,7 @@ public class DatabaseEngine
             closeConnection(rs, s, conn);
         }
     }
-    private void addYearData(int year, AnalysisEngine e) {
+    private void addOldYearData(int year, AnalysisEngine e) {
         int index = (e.years.size() - 1) - e.years.indexOf(year);
         int currentAssetYear = 0, currentAssetIndex = 0;
 
@@ -67,49 +67,31 @@ public class DatabaseEngine
         try {
             conn = createConnection();
             s = conn.createStatement();
-            int buys = 0, sells = 0;
 
-            for (int i = 0; i < e.insiderBuys.size(); i++) {
-                buys += e.insiderBuys.get(i);
-            }
-            for (int i = 0; i < e.insiderSells.size(); i++) {
-                sells += e.insiderSells.get(i);
-            }
-            for (int i = 0; i < e.currentAssets.size(); i++) {
-                if (e.currentPositionYears.get(i).equals(year)) {
-                    currentAssetYear = Integer.parseInt(String.valueOf(e.currentPositionYears.get(i)));
-                    currentAssetIndex = i;
-                }
+            sql =   "INSERT INTO Stocks.StockQuarter (" +
+                    "YearID, QuarterID, StockID, High, Low, Revenues Per Share, Cash Flow Per Share, Earnings Per Share, " +
+                    "Book Value Per Share, Average Annual P/E Ratio, Average Annual Dividend Yield, Revenues, Net Profit, " +
+                    "Net Profit Margin, Long-term Debt, Return on Share Equity";
+            if (currentAssetYear == year) {
+                sql += ", Current Assets, Current Liability ";
             }
 
-            if (year != e.year) {
-                sql =   "INSERT INTO Stocks.StockQuarter (" +
-                        "YearID, QuarterID, StockID, Price, P/E, Dividend Yield, Timeliness, Safety, HighProj, HighProjGain, " +
-                        "HighProjAnnualReturn, LowProj, InsiderBuys, InsiderSells, Revenues Per Share, Cash Flow Per Share, Earnings Per Share, " +
-                        "Book Value Per Share, Average Annual P/E Ratio, Average Annual Dividend Yield, Revenues, Net Profit, " +
-                        "Net Profit Margin, Long-term Debt, Return on Share Equity";
-                if (currentAssetYear == year) {
-                    sql += ", Current Assets, Current Liability ";
-                }
-
-                sql +=  "VALUES (" +
-                        "SELECT Stocks.Years.ID FROM Stocks.Years WHERE Stocks.Years.YearValue = " + year + ", " +
-                        "SELECT Stocks.Quarters.ID FROM Stocks.Quarters WHERE Stocks.Quarters.QuarterValue = " + e.quarter + ", " +
-                        "SELECT Stocks.Stock.ID FROM Stocks.Stock WHERE Stocks.Stock.Symbol = " + e.symbol + ", " +
-                        "" + e.recentPrice + ", " + e.PERatio + ", " + e.dividendYield + ", " + e.timeliness + ", " + e.safety + ", " +
-                        "" + e.highProjections[0] + ", " + e.highProjections[1] + ", " + e.highProjections[2] + ", " + e.lowProjections[0] + ", " +
-                        "" + e.lowProjections[1] + ", " + e.lowProjections[2] + ", " + buys + ", " + sells + ", " +
-                        "" + e.revenuesPerShare.get(e.revenuesPerShare.size() - index) + ", " + e.cashFlowPerShare.get(e.cashFlowPerShare.size() - index) + ", " +
-                        "" + e.earningsPerShare.get(e.earningsPerShare.size() - index) + ", " + e.bookValuePerShare.get(e.bookValuePerShare.size() - index) + ", " +
-                        "" + e.averageAnnualPERatio.get(e.averageAnnualPERatio.size() - index) + ", " + e.averageAnnualDividendYield.get(e.averageAnnualDividendYield.size() - index) + ", " +
-                        "" + e.revenues.get(e.revenues.size() - index) + ", " + e.netProfit.get(e.netProfit.size() - index) + ", " +
-                        "" + e.netProfitMargin.get(e.netProfitMargin.size() - index) + ", " + e.longTermDebt.get(e.longTermDebt.size() - index) + ", " +
-                        "" + e.returnOnShareEquity.get(e.returnOnShareEquity.size() - index);
-                if (currentAssetYear == year) {
-                    sql += ", " + e.currentAssets.get(currentAssetIndex) + ", " + e.currentLiability.get(currentAssetIndex);
-                }
-                sql +=  ");";
+            sql +=  "VALUES (" +
+                    "SELECT Stocks.Years.ID FROM Stocks.Years WHERE Stocks.Years.YearValue = " + year + ", " +
+                    "SELECT Stocks.Quarters.ID FROM Stocks.Quarters WHERE Stocks.Quarters.QuarterValue = " + 0 + ", " +
+                    "SELECT Stocks.Stock.ID FROM Stocks.Stock WHERE Stocks.Stock.Symbol = " + e.symbol + ", " +
+                    "" + e.yearHighs.get(e.yearHighs.size() - index) + ", " + e.yearLows.get(e.yearLows.size() - index) + ", " +
+                    "" + e.revenuesPerShare.get(e.revenuesPerShare.size() - index) + ", " + e.cashFlowPerShare.get(e.cashFlowPerShare.size() - index) + ", " +
+                    "" + e.earningsPerShare.get(e.earningsPerShare.size() - index) + ", " + e.bookValuePerShare.get(e.bookValuePerShare.size() - index) + ", " +
+                    "" + e.averageAnnualPERatio.get(e.averageAnnualPERatio.size() - index) + ", " + e.averageAnnualDividendYield.get(e.averageAnnualDividendYield.size() - index) + ", " +
+                    "" + e.revenues.get(e.revenues.size() - index) + ", " + e.netProfit.get(e.netProfit.size() - index) + ", " +
+                    "" + e.netProfitMargin.get(e.netProfitMargin.size() - index) + ", " + e.longTermDebt.get(e.longTermDebt.size() - index) + ", " +
+                    "" + e.returnOnShareEquity.get(e.returnOnShareEquity.size() - index);
+            if (currentAssetYear == year) {
+                sql += ", " + e.currentAssets.get(currentAssetIndex) + ", " + e.currentLiability.get(currentAssetIndex);
             }
+            sql +=  ");";
+            /*
             else {
                 sql =   "INSERT INTO Stocks.StockQuarter (" +
                         "YearID, QuarterID, StockID, Price, P/E, Dividend Yield, Timeliness, Safety, HighProj, HighProjGain, " +
@@ -141,7 +123,7 @@ public class DatabaseEngine
                         "" + e.annualBookValue.get(0) + ", " + e.annualBookValue.get(1) + ", " + e.annualBookValue.get(2) + ", " +
                         "" + e.companyStrength + ", " + e.priceStability + ", " + e.growthPersistence + ", " + e.predictability +
                 ");";
-            }
+            }*/
             s.executeUpdate(sql);
         }
         catch (Exception ex) {
