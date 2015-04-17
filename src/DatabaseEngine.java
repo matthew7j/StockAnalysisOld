b883d82444015c3129bebf73b6ce28b60793dfa4
mjones <matthew7j@aol.com>
2015-04-17 16:20:04 -0400

    Adding method for quarterly information

--- src/DatabaseEngine.java
+++ src/DatabaseEngine.java
@@ -93,7 +93,7 @@ public class DatabaseEngine
                 sells += e.insiderSells.get(i);
             }
 
-            sql =   "INSERT INTO Stocks.StockQuarter (" +
+            sql =   "INSERT INTO Stocks.QuarterData (" +
                     "YearID, QuarterID, StockID, Price, P/E, Dividend Yield, Timeliness, Safety, HighProj, HighProjGain, " +
                     "HighProjAnnualReturn, LowProj, " +
                     "InsiderBuys, InsiderSells, Revenues Per Share, Cash Flow Per Share, Earnings Per Share, " +
@@ -148,7 +148,7 @@ public class DatabaseEngine
             conn = createConnection();
             s = conn.createStatement();
 
-            sql =   "INSERT INTO Stocks.StockQuarter (" +
+            sql =   "INSERT INTO Stocks.YearData (" +
                     "YearID, QuarterID, StockID, High, Low, Revenues Per Share, Cash Flow Per Share, Earnings Per Share, " +
                     "Book Value Per Share, Average Annual P/E Ratio, Average Annual Dividend Yield, Revenues, Net Profit, " +
                     "Net Profit Margin, Long-term Debt, Return on Share Equity";
@@ -171,39 +171,6 @@ public class DatabaseEngine
                 sql += ", " + e.currentAssets.get(currentAssetIndex) + ", " + e.currentLiability.get(currentAssetIndex);
             }
             sql +=  ");";
-            /*
-            else {
-                sql =   "INSERT INTO Stocks.StockQuarter (" +
-                        "YearID, QuarterID, StockID, Price, P/E, Dividend Yield, Timeliness, Safety, HighProj, HighProjGain, " +
-                        "HighProjAnnualReturn, LowProj, " +
-                        "InsiderBuys, InsiderSells, Revenues Per Share, Cash Flow Per Share, Earnings Per Share, " +
-                        "Book Value Per Share, Average Annual P/E Ratio, Average Annual Dividend Yield, Revenues, Net Profit, " +
-                        "Net Profit Margin, Long-term Debt, Return on Share Equity, Total Debt, Market Cap, Current Assets, " +
-                        "Current Liabilities, Annual Revenues Past 10, Annual Revenues Past 5, Annual Revenues Future 5, " +
-                        "Annual Earnings Past 10, Annual Earnings Past 5, Annual Earnings Future 5,  Annual Dividends Past 10, " +
-                        "Annual Dividends Past 5, Annual Dividends Future 5, Annual Book Value Past 10, Annual Book Value Past 5, " +
-                        "Annual BOok Value Future 5, Strength, Price Stability, Growth Persistence, Earnings Predictability) " +
-                        "VALUES (" +
-                        "SELECT Stocks.Years.ID FROM Stocks.Years WHERE Stocks.Years.YearValue = " + year + ", " +
-                        "SELECT Stocks.Quarters.ID FROM Stocks.Quarters WHERE Stocks.Quarters.QuarterValue = " + e.quarter + ", " +
-                        "SELECT Stocks.Stock.ID FROM Stocks.Stock WHERE Stocks.Stock.Symbol = " + e.symbol + ", " +
-                        "" + e.recentPrice + ", " + e.PERatio + ", " + e.dividendYield + ", " + e.timeliness + ", " + e.safety + ", " +
-                        "" + e.highProjections[0] + ", " + e.highProjections[1] + ", " + e.highProjections[2] + ", " + e.lowProjections[0] + ", " +
-                        "" + e.lowProjections[1] + ", " + e.lowProjections[2] + ", " + buys + ", " + sells + ", " +
-                        "" + e.revenuesPerShare.get(e.revenuesPerShare.size() - index) + ", " + e.cashFlowPerShare.get(e.cashFlowPerShare.size() - index) + ", " +
-                        "" + e.earningsPerShare.get(e.earningsPerShare.size() - index) + ", " + e.bookValuePerShare.get(e.bookValuePerShare.size() - index) + ", " +
-                        "" + e.averageAnnualPERatio.get(e.averageAnnualPERatio.size() - index) + ", " + e.averageAnnualDividendYield.get(e.averageAnnualDividendYield.size() - index) + ", " +
-                        "" + e.revenues.get(e.revenues.size() - index) + ", " + e.netProfit.get(e.netProfit.size() - index) + ", " +
-                        "" + e.netProfitMargin.get(e.netProfitMargin.size() - index) + ", " + e.longTermDebt.get(e.longTermDebt.size() - index) + ", " +
-                        "" + e.returnOnShareEquity.get(e.returnOnShareEquity.size() - index) + ", " + e.totalDebt + ", " + e.marketCap + ", " +
-                        "" + e.currentAssets.get(e.currentAssets.size() - 1) + ", " + e.currentLiability.get(e.currentLiability.size() - 1) + ", " +
-                        "" + e.annualRevenues.get(0) + ", " + e.annualRevenues.get(1) + ", " + e.annualRevenues.get(2) + ", " +
-                        "" + e.annualEarnings.get(0) + ", " + e.annualEarnings.get(1) + ", " + e.annualEarnings.get(2) + ", " +
-                        "" + e.annualDividends.get(0) + ", " + e.annualDividends.get(1) + ", " + e.annualDividends.get(2) + ", " +
-                        "" + e.annualBookValue.get(0) + ", " + e.annualBookValue.get(1) + ", " + e.annualBookValue.get(2) + ", " +
-                        "" + e.companyStrength + ", " + e.priceStability + ", " + e.growthPersistence + ", " + e.predictability +
-                ");";
-            }*/
             s.executeUpdate(sql);
         }
         catch (Exception ex) {
